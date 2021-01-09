283eec05790af095c89f1ef57faa28d2d26b726d
jay-jajj <asjh011969@gmail.com>
2021-01-09 10:22:40 +0900

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jay:soclsrn1@sign.ru6qq.mongodb.net/<dbname>?retryWrites=true&w=majority',
+const app = express();
+const port = 4000;
+
+const {User} = require('./models/user')
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(bodyParser.json());
+
+
+
+mongoose.connect(config.mongoURI,
 {
     useNewUrlParser : true,
     useUnifiedTopology : true,
@@ -9,3 +24,24 @@ mongoose.connect('mongodb+srv://jay:soclsrn1@sign.ru6qq.mongodb.net/<dbname>?ret
 }).then(()=> console.log("MongoDB Connected Successfully!!!"))
 .catch(err => console.log(err));
 
+
+
+
+
+
+app.get('/', (req, res) => res.send('Ok!'));
+
+
+app.post('/register', (req, res) => {
+//회원 가입시 필요한 정보들을 clien에서 가져오면
+//db에 저장
+ const user = new User(req.body);
+ user.save((err, doc) => {
+     if(err) return res.json({success: false, err});
+     return res.status(200).json({success: true});
+ });
+});
+
+
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
